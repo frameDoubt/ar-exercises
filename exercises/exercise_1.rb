c03f06f71f48792c797488dbc4033be488bef5cb
Brandon Wright <brandon.w.wright4@gmail.com>
2021-05-27 19:47:53 +0000

    add store to stores database

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,21 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+# class Stores < ApplicationRecord
+# end
+
+
+Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+
+# first_store.save
+
+# second_store= Stores.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
+
+# second_store.save
+
+# third_store= Stores.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+# second_store.save
+
+puts Store.count
